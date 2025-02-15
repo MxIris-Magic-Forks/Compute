ce9907e5d8bdb3541360dcd4b512cb77324bcab3
James Moschou <james.moschou@gmail.com>
2025-02-16 10:37:24 +1100

    Implement profiler methods on Graph

---
+++ Sources/Utilities/include/Utilities/FreeDeleter.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <CoreFoundation/CFBase.h>
+
+CF_ASSUME_NONNULL_BEGIN
+
+namespace util {
+
+class free_deleter {
+  public:
+    template <typename T> void operator()(T *_Nullable p) {
+        if (p) {
+            free((void *)p);
+        }
+    }
+};
+
+} // namespace util
+
+CF_ASSUME_NONNULL_END
